9c162c9fd7e21c010cc332376bc7731a86754b05
Tomek Rozalski <github@tomaszrozalski.pl>
2023-05-18 21:02:03 +0200

    [login] Try to deploy

--- src/routes/[[lang=langCode]]/login/+page.server.ts
+++ src/routes/[[lang=langCode]]/login/+page.server.ts
@@ -1,17 +1,17 @@
 import { fail } from '@sveltejs/kit';
 import { superValidate } from 'sveltekit-superforms/server';
-import schema from './LoginForm/validationSchema';
+import validationSchema from './LoginForm/validationSchema';
 import onSubmit from './LoginForm/onSubmit';
 
 export const load = async () => {
-	const form = await superValidate(schema);
+	const form = await superValidate(validationSchema);
 
 	return { form };
 };
 
 export const actions = {
 	default: async (event) => {
-		const form = await superValidate(event.request, schema);
+		const form = await superValidate(event.request, validationSchema);
 
 		if (!form.valid) {
 			return fail(400, { form });
