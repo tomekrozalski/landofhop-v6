fb7889a64f9613988782fd6ce056bb015c477e56
Tomek Rozalski <github@tomaszrozalski.pl>
2023-07-03 12:45:02 +0200

    [details] Refactoring of breadcrumbs. Try to fix build

--- src/lib/utils/api/getBreadcrumbPhrase.ts
+++ src/lib/utils/api/getBreadcrumbPhrase.ts
@@ -1,8 +1,8 @@
 import { format } from 'date-fns';
-import locale from 'date-fns/locale';
+import pl from 'date-fns/locale/pl/index.js';
 import type { Locales } from '$lib/i18n/i18n-types';
 
 const getLatestMonth = (date: Date, language: Locales) =>
-	format(date, 'LLLL yyyy', { locale: language === 'pl' ? locale.pl : undefined });
+	format(date, 'LLLL yyyy', { locale: language === 'pl' ? pl : undefined });
 
 export default getLatestMonth;
