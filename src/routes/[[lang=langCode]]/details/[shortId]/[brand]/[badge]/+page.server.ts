8f4af5b3876f43940cc049797ac018dc18ca8208
Tomek Rozalski <github@tomaszrozalski.pl>
2023-10-30 17:36:37 +0100

    [configuration] Incremental Static Regeneration test 7

--- src/routes/[[lang=langCode]]/details/[shortId]/[brand]/[badge]/+page.server.ts
+++ src/routes/[[lang=langCode]]/details/[shortId]/[brand]/[badge]/+page.server.ts
@@ -4,7 +4,11 @@ import { getBreadcrumbPhrase, getLatestMonth } from '$lib/utils/api';
 import { detailsNormalizer, getAdminData, getDetails, getNext, getPrevious } from './utils/load';
 
 export const config = {
-	runtime: 'edge'
+	runtime: 'edge',
+	isr: {
+		expiration: 60,
+		bypassToken: 'REPLACE_ME_WITH_SECRET_VALUE'
+	}
 };
 
 export const load = async ({ locals: { authenticated, locale }, params }) => {
