5bf8ee91194849f1b8b23824a4667baef16b4cfa
Tomek Rozalski <github@tomaszrozalski.pl>
2023-10-30 14:54:22 +0100

    [configuration] Incremental Static Regeneration test 5

--- src/routes/[[lang=langCode]]/details/[shortId]/[brand]/[badge]/+page.server.ts
+++ src/routes/[[lang=langCode]]/details/[shortId]/[brand]/[badge]/+page.server.ts
@@ -3,10 +3,6 @@ import { error } from '@sveltejs/kit';
 import { getBreadcrumbPhrase, getLatestMonth } from '$lib/utils/api';
 import { detailsNormalizer, getAdminData, getDetails, getNext, getPrevious } from './utils/load';
 
-export const config = {
-	isr: { expiration: 60 }
-};
-
 export const load = async ({ locals: { authenticated, locale }, params }) => {
 	const badge = params.badge;
 	const shortId = params.shortId;
---
+++ src/routes/[[lang=langCode]]/details/[shortId]/[brand]/[badge]/+page.ts
@@ -0,0 +1,3 @@
+export const config = {
+	isr: { expiration: 60 }
+};
