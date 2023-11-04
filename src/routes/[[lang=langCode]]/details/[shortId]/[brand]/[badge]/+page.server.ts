06539e86429957580c09593774ff87b1d640dedd
Tomek Rozalski <github@tomaszrozalski.pl>
2023-11-04 17:03:31 +0100

    [configuration] Try to debug Vercel ECONNRESET, part 5

--- src/routes/[[lang=langCode]]/details/[shortId]/[brand]/[badge]/+page.server.ts
+++ src/routes/[[lang=langCode]]/details/[shortId]/[brand]/[badge]/+page.server.ts
@@ -19,13 +19,13 @@ export const load = async ({ locals: { authenticated, locale }, params }) => {
 
 	try {
 		const beverage = await getDetails({ shortId });
-		const formattedDetails = detailsNormalizer(beverage, locale);
+		// const formattedDetails = detailsNormalizer(beverage, locale);
 		// const latestMonth = await getLatestMonth();
 		// const addedDate = new Date(beverage.added);
 
 		return {
 			// previous: getPrevious(beverage.added),
-			details: formattedDetails
+			details: beverage
 			// breadcrumbs: {
 			// 	link:
 			// 		format(addedDate, 'M-yyyy') === format(latestMonth, 'M-yyyy')
