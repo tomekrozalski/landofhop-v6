cc9b5fe4cdf617469e517230f846e7a97daf9560
Tomek Rozalski <github@tomaszrozalski.pl>
2023-11-04 17:50:13 +0100

    [configuration] Try to debug Vercel ECONNRESET, part 9

--- src/routes/[[lang=langCode]]/details/[shortId]/[brand]/[badge]/+page.server.ts
+++ src/routes/[[lang=langCode]]/details/[shortId]/[brand]/[badge]/+page.server.ts
@@ -1,40 +1,34 @@
-// import { format } from 'date-fns';
-// import { error } from '@sveltejs/kit';
-// import { getBreadcrumbPhrase, getLatestMonth } from '$lib/utils/api';
-// import { detailsNormalizer, getAdminData, getDetails, getNext, getPrevious } from './utils/load';
+import { format } from 'date-fns';
+import { error } from '@sveltejs/kit';
+import { getBreadcrumbPhrase, getLatestMonth } from '$lib/utils/api';
+import { detailsNormalizer, getAdminData, getDetails, getNext, getPrevious } from './utils/load';
 
-// export const config = {
-// 	isr: {
-// 		expiration: 600
-// 	}
-// };
+export const load = async ({ locals: { authenticated, locale }, params }) => {
+	const badge = params.badge;
+	const shortId = params.shortId;
 
-export const load = async () => {
-	// 	const badge = params.badge;
-	// 	const shortId = params.shortId;
-	//
-	// 	if (!badge || !shortId) {
-	// 		throw error(404, 'Incorrect params');
-	// 	}
+	if (!badge || !shortId) {
+		throw error(404, 'Incorrect params');
+	}
 
-	// const beverage = await getDetails({ shortId });
-	// const formattedDetails = detailsNormalizer(beverage, locale);
-	// const latestMonth = await getLatestMonth();
-	// const addedDate = new Date(beverage.added);
+	const beverage = await getDetails({ shortId });
+	const formattedDetails = detailsNormalizer(beverage, locale);
+	const latestMonth = await getLatestMonth();
+	const addedDate = new Date(beverage.added);
 
 	return {
-		// previous: getPrevious(beverage.added),
-		details: { dupa: 'test' }
-		// breadcrumbs: {
-		// 	link:
-		// 		format(addedDate, 'M-yyyy') === format(latestMonth, 'M-yyyy')
-		// 			? '/'
-		// 			: `/list/${format(addedDate, 'yyyy-MM')}`,
-		// 	phrase: getBreadcrumbPhrase(addedDate, locale)
-		// },
-		// next: getNext(beverage.added),
-		// streamed: {
-		// 	...(authenticated && { adminData: getAdminData(shortId, locale) })
-		// }
+		previous: getPrevious(beverage.added),
+		details: formattedDetails,
+		breadcrumbs: {
+			link:
+				format(addedDate, 'M-yyyy') === format(latestMonth, 'M-yyyy')
+					? '/'
+					: `/list/${format(addedDate, 'yyyy-MM')}`,
+			phrase: getBreadcrumbPhrase(addedDate, locale)
+		},
+		next: getNext(beverage.added),
+		streamed: {
+			...(authenticated && { adminData: getAdminData(shortId, locale) })
+		}
 	};
 };
