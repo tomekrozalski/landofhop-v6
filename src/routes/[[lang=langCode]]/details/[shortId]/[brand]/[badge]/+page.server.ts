e8643ae268bcb3ac7046892057b3ff24a4c104ab
Tomek Rozalski <github@tomaszrozalski.pl>
2023-11-05 18:08:22 +0100

    [configuration] Last attempt to implement Incremental Static Regeneration, part 4

--- src/routes/[[lang=langCode]]/details/[shortId]/[brand]/[badge]/+page.server.ts
+++ src/routes/[[lang=langCode]]/details/[shortId]/[brand]/[badge]/+page.server.ts
@@ -3,10 +3,6 @@ import { error } from '@sveltejs/kit';
 import { getBreadcrumbPhrase, getLatestMonth } from '$lib/utils/api';
 import { detailsNormalizer, getAdminData, getDetails, getNext, getPrevious } from './utils/load';
 
-export const config = {
-	runtime: 'edge'
-};
-
 export const load = async ({ locals: { authenticated, locale }, params }) => {
 	const badge = params.badge;
 	const shortId = params.shortId;
