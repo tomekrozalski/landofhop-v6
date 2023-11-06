22fc1bea734bb22620337ce37d7ae715b882e8f1
Tomek Rozalski <github@tomaszrozalski.pl>
2023-11-06 07:44:06 +0100

    [configuration] Uninstall html-minifier

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,29 +1,9 @@
 import { redirect } from '@sveltejs/kit';
-// import { minify } from 'html-minifier';
 import { i18n } from '$lib/i18n/i18n-util';
 import type { Locales } from '$lib/i18n/i18n-types';
 import { loadAllLocales } from '$lib/i18n/i18n-util.sync';
 import { start_mongo } from '$lib/db/mongo';
 import { authenticate } from '$lib/utils/api';
-
-// const minification_options = {
-// 	collapseBooleanAttributes: true,
-// 	collapseWhitespace: true,
-// 	conservativeCollapse: true,
-// 	decodeEntities: true,
-// 	html5: true,
-// 	ignoreCustomComments: [/^#/],
-// 	minifyCSS: true,
-// 	minifyJS: false,
-// 	removeAttributeQuotes: true,
-// 	removeComments: true,
-// 	removeOptionalTags: true,
-// 	removeRedundantAttributes: true,
-// 	removeScriptTypeAttributes: true,
-// 	removeStyleLinkTypeAttributes: true,
-// 	sortAttributes: true,
-// 	sortClassName: true
-// };
 
 start_mongo()
 	.then(() => {
@@ -68,19 +48,6 @@ export const handle = async ({ event, resolve }) => {
 	const response = await resolve(event, {
 		transformPageChunk: ({ html }) => html.replace('%lang%', locale)
 	});
-
-	/* ---------------*/
-	/* Minification */
-
-	// const mode = process.env.NODE_ENV;
-	// const dev = mode === 'development';
-
-	// if (!dev && response.headers.get('content-type') === 'text/html') {
-	// 	return new Response(minify(await response.text(), minification_options), {
-	// 		status: response.status,
-	// 		headers: response.headers
-	// 	});
-	// }
 
 	return response;
 };
