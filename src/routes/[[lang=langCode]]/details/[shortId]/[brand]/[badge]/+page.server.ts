fa256668dc86c695d5886513094e277a5f7d6426
Tomek Rozalski <github@tomaszrozalski.pl>
2024-01-14 19:37:09 +0100

    [details] Try to fix navigation

--- src/routes/[[lang=langCode]]/details/[shortId]/[brand]/[badge]/+page.server.ts
+++ src/routes/[[lang=langCode]]/details/[shortId]/[brand]/[badge]/+page.server.ts
@@ -23,7 +23,7 @@ export const load = async ({ locals: { authenticated, locale }, params }) => {
 	const addedDate = new Date(beverage.added);
 
 	return {
-		previous: getPrevious(beverage.added),
+		previous: await getPrevious(beverage.added),
 		details: formattedDetails,
 		breadcrumbs: {
 			link:
@@ -32,7 +32,7 @@ export const load = async ({ locals: { authenticated, locale }, params }) => {
 					: `/list/${format(addedDate, 'yyyy-MM')}`,
 			phrase: getBreadcrumbPhrase(addedDate, locale)
 		},
-		next: getNext(beverage.added),
+		next: await getNext(beverage.added),
 		streamed: {
 			...(authenticated && { adminData: getAdminData(shortId, locale) })
 		}
