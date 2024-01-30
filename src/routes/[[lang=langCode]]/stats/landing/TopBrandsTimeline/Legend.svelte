20784c378c1e2e250413175268641db4b6041fc4
Tomek Rozalski <github@tomaszrozalski.pl>
2024-01-30 14:46:59 +0100

    [stats] Work on most popular brands timeline legend

--- src/routes/[[lang=langCode]]/stats/landing/TopBrandsTimeline/Legend.svelte
+++ src/routes/[[lang=langCode]]/stats/landing/TopBrandsTimeline/Legend.svelte
@@ -7,25 +7,61 @@
 	export let topBrandsTimelineData: TopBrandsTimelineBar[];
 </script>
 
-<ol class="flex flex-col gap-1 px-3 lg:flex-row lg:flex-wrap">
+<ol class="mb-10 ml-12 flex flex-col gap-1 px-3 lg:flex-row lg:flex-wrap">
 	{#each topBrandsTimelineData[topBrandsTimelineData.length - 1].brands as { amount, badge, id, name }, index}
-		<li style="counter-increment: item">
+		<li>
 			<a
-				class="inline-block rounded top-brands-legend-{index + 1} pr-1"
-				class:highlighted={id === selectedBrand}
+				class="flex rounded pr-2 transition-colors"
+				class:text-white={id === selectedBrand}
+				class:bg-red={index === 0 && id === selectedBrand}
+				class:bg-orange={index === 1 && id === selectedBrand}
+				class:bg-yellow={index === 2 && id === selectedBrand}
+				class:bg-green={index === 3 && id === selectedBrand}
+				class:bg-sea={index === 4 && id === selectedBrand}
+				class:bg-turquoise={index === 5 && id === selectedBrand}
+				class:bg-red-light={index === 6 && id === selectedBrand}
+				class:bg-gray-600={index === 7 && id === selectedBrand}
+				class:bg-gray-400={index === 8 && id === selectedBrand}
+				class:bg-gray-300={index === 9 && id === selectedBrand}
 				href="/brand/{id}/{badge}"
 				on:mouseenter={() => (selectedBrand = id)}
 				on:mouseleave={() => (selectedBrand = null)}
 			>
-				{name.value} <span>({amount})</span>
+				<i
+					class="mr-2 flex items-center justify-center rounded border-r px-3 text-sm not-italic text-white"
+					class:rounded-r-none={id === selectedBrand}
+					class:bg-red={index === 0}
+					class:bg-orange={index === 1}
+					class:bg-yellow={index === 2}
+					class:bg-green={index === 3}
+					class:bg-sea={index === 4}
+					class:bg-turquoise={index === 5}
+					class:bg-red-light={index === 6}
+					class:bg-gray-600={index === 7}
+					class:bg-gray-400={index === 8}
+					class:bg-gray-300={index === 9}
+					role="presentation"
+				>
+					{index + 1}
+				</i>
+				{name.value}
+				<span
+					class="mb-0.5 ml-1 self-end text-sm transition-colors"
+					class:text-gray-700={id !== selectedBrand}
+					class:text-white={id === selectedBrand}
+				>
+					({amount})
+				</span>
 			</a>
 		</li>
 	{/each}
 </ol>
 
-<h3>{$LL.pages.stats.landing.topBrandsTimeline.morePopularBrands()}</h3>
+<h3 class="my-5 pl-16 font-medium">
+	{$LL.pages.stats.landing.topBrandsTimeline.morePopularBrands()}
+</h3>
 
-<ul class="flex list-disc flex-col gap-1 px-3 lg:flex-row lg:flex-wrap">
+<ul class="flex list-inside list-disc flex-col gap-1 px-12 lg:flex-row lg:flex-wrap">
 	{#each morePopularBrandsData as { amount, badge, id, name }, index}
 		<li>
 			<a
@@ -33,122 +69,8 @@
 				class="hover:underline"
 				class:pr-2={index === morePopularBrandsData.length - 1}
 			>
-				{name.value} <span>({amount})</span>
+				{name.value} <span class="text-sm">({amount})</span>
 			</a>
 		</li>
 	{/each}
 </ul>
-
-<style>
-	ul a::before {
-		display: inline-block;
-		content: '';
-		width: 0.6rem;
-		height: 0.6rem;
-		margin: 0 0.8rem 0.2rem 0.2rem;
-		background-color: var(--color-black);
-	}
-
-	ol a::before {
-		display: inline-flex;
-		justify-content: center;
-		align-items: center;
-		width: 3rem;
-		height: 3rem;
-		margin-right: 0.4rem;
-		border-radius: 0.8rem;
-		content: counter(item);
-		color: var(--color-white);
-	}
-
-	li:not(:last-of-type) a::after {
-		content: ',';
-	}
-
-	.highlighted {
-		color: var(--color-white);
-	}
-
-	.highlighted::after {
-		color: transparent;
-	}
-
-	ol li:last-of-type::after {
-		content: '';
-		padding-right: 0.8rem;
-	}
-
-	.top-brands-legend-1.highlighted,
-	.top-brands-legend-1::before {
-		background-color: var(--color-brand-1);
-	}
-
-	.top-brands-legend-2.highlighted,
-	.top-brands-legend-2::before {
-		background-color: var(--color-brand-3);
-	}
-
-	.top-brands-legend-3.highlighted,
-	.top-brands-legend-3::before {
-		background-color: var(--color-brand-5);
-	}
-
-	.top-brands-legend-4.highlighted,
-	.top-brands-legend-4::before {
-		background-color: var(--color-brand-6);
-	}
-
-	.top-brands-legend-5.highlighted,
-	.top-brands-legend-5::before {
-		background-color: var(--color-brand-8);
-	}
-
-	.top-brands-legend-6.highlighted,
-	.top-brands-legend-6::before {
-		background-color: var(--color-brand-10);
-	}
-
-	.top-brands-legend-7.highlighted,
-	.top-brands-legend-7::before {
-		background-color: #555;
-	}
-
-	.top-brands-legend-8.highlighted,
-	.top-brands-legend-8::before {
-		background-color: #666;
-	}
-
-	.top-brands-legend-9.highlighted,
-	.top-brands-legend-9::before {
-		background-color: #999;
-	}
-
-	.top-brands-legend-10.highlighted,
-	.top-brands-legend-10::before {
-		background-color: #ccc;
-	}
-
-	span {
-		font-size: 1.4rem;
-		font-weight: var(--font-weight-light);
-	}
-
-	h3 {
-		margin: 4rem 0 2rem 0;
-		padding-left: 1rem;
-		font-weight: var(--font-weight-regular);
-	}
-
-	@media (min-width: 768px) {
-		/* ol,
-		ul {
-			flex-direction: row;
-			flex-wrap: wrap;
-			padding: 0 4rem;
-		} */
-
-		h3 {
-			padding-left: 4rem;
-		}
-	}
-</style>
