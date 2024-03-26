0256dd9b532ab05e9709ab0b9deb0c29e2ebadd4
Tomek Rozalski <github@tomaszrozalski.pl>
2024-03-26 20:40:23 +0100

    [dashboard] RemoveRow and learn new Voyager keyboard

--- src/lib/atoms/forms/RemoveRow.svelte
+++ src/lib/atoms/forms/RemoveRow.svelte
@@ -2,13 +2,13 @@
 	import { formFieldProxy } from 'sveltekit-superforms';
 	import type { SuperForm, FormPathLeaves } from 'sveltekit-superforms';
 	import Fa from 'svelte-fa';
-	import { faPlus } from '@fortawesome/pro-solid-svg-icons';
+	import { faMinus } from '@fortawesome/pro-solid-svg-icons';
 
-	let { field, form }: { field: string; form: SuperForm<T> } = $props();
+	let { field, form, index }: { field: string; form: SuperForm<T>; index: number } = $props();
 	const { value } = formFieldProxy(form, field as FormPathLeaves<T>);
 
 	function onClick() {
-		$value.push({ value: '', language: '' });
+		$value.splice(index, 1);
 		$value = $value;
 	}
 </script>
@@ -19,5 +19,5 @@
 	class="flex aspect-square h-full items-center justify-center justify-self-start bg-gray-100 text-gray-300 transition-colors
   hover:bg-black hover:text-white focus-visible:bg-black focus-visible:text-white"
 >
-	<Fa icon={faPlus} />
+	<Fa icon={faMinus} />
 </button>
--- src/lib/molecules/forms/fields/Name.svelte
+++ src/lib/molecules/forms/fields/Name.svelte
@@ -5,6 +5,7 @@
 	import Label from '$lib/atoms/forms/Label.svelte';
 	import TextInput from '$lib/atoms/forms/TextInput.svelte';
 	import AddRow from '$lib/atoms/forms/AddRow.svelte';
+	import RemoveRow from '$lib/atoms/forms/RemoveRow.svelte';
 	import LanguageSelect from '../selects/Language.svelte';
 	import Grid from '../Grid.svelte';
 
@@ -17,14 +18,19 @@
 	<Label class="col-span-2" field="name" formId={$formId} isRequired>
 		{$LL.pages.dashboard.label.name()}
 	</Label>
-	{@const  abd = $value as Array<{ value: string; language: string }>}
-	{#each abd as _, i}
+	{@const  names = $value as { value: string; language: string }[]}
+	{#each names as _, i}
 		<div class="col-start-3 grid grid-cols-2 gap-2">
 			<TextInput field="name[{i}].value" {form} />
 			<LanguageSelect field="name[{i}].language" {form} />
 		</div>
-		{#if abd.length === i + 1}
-			<AddRow {form} field="name" />
-		{/if}
+		<div class="flex gap-2">
+			{#if names.length > 1}
+				<RemoveRow {form} field="name" index={i} />
+			{/if}
+			{#if names.length === i + 1}
+				<AddRow {form} field="name" />
+			{/if}
+		</div>
 	{/each}
 </Grid>
